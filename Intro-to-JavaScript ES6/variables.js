49d28a24f3f46adde0e5625d212965b4e2d59bba
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-07 08:23:16 +0530

    Create variables.js

---
+++ Intro-to-JavaScript ES6/variables.js
@@ -0,0 +1,8 @@
+// User Input
+prompt("What is your name?");
+
+// Variables
+var myName = "Aditya";
+// alert(name);
+var yourName = prompt("What is your name?");
+alert("My name is " + myName + ", welcome to my profile " + yourName + "!");
