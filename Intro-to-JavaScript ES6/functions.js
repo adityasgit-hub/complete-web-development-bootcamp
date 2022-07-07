8321256916ee2bf23e42cd830b1d2acf4dc2fdc1
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-07 13:34:45 +0530

    Create functions.js

---
+++ Intro-to-JavaScript ES6/functions.js
@@ -0,0 +1,22 @@
+// Let's say we have a robot and we want it to go and get some milk for us.
+// So, instead of giving the robot commands each day, we can just simply create a function in which we can put in all the commands in order for our robot to get milk.
+// So, instead of writing commands everyday, we can just call the function.
+
+// Creating the function
+function getMilk() {
+  console.log("command-1");
+  console.log("command-2");
+  console.log("command-3");
+  console.log("command-4");
+  console.log("command-5");
+  console.log("command-6");
+  console.log("command-7");
+  console.log("command-8");
+  console.log("command-9");
+  console.log("command-10");
+  console.log("command-11");
+  console.log("command-12");
+}
+
+// Calling the function
+getMilk();
