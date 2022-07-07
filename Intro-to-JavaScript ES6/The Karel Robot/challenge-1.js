897e63e845b68e5449d6f432a2034a1ced2a03c5
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-07 13:41:27 +0530

    Create challenge-1.js

---
+++ Intro-to-JavaScript ES6/The Karel Robot/challenge-1.js
@@ -0,0 +1,18 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+ 
+function main() {
+   moveFourTimes();
+   turnLeft();
+   moveFourTimes();
+}
+
+function moveFourTimes() {
+   move();
+   move();
+   move();
+   move();
+}
