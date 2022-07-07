f234d6d11772758a60681c46b35dfc0813391223
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-07 21:33:24 +0530

    Create life-in-weeks.js

---
+++ Intro-to-JavaScript ES6/life-in-weeks.js
@@ -0,0 +1,8 @@
+var age = prompt("How old are you?");
+// var yearsLeft = 90 - age;
+// var monthsLeft = yearsLeft * 12;
+// var weeksLeft = yearsLeft * 52;
+// var daysLeft = yearsLeft * 365;
+// console.log("You have " + daysLeft + " days, " + weeksLeft + " weeks, and " + monthsLeft + " months left.");
+
+console.log("You have " + ((90 - age) * 365) + " days, " + ((90 - age) * 52) + " weeks, and " + ((90 - age) * 12) + " months left.");
