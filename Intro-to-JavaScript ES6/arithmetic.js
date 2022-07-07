ef415652d69fd26e1c2ddc08c0fe7bd821049054
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-07 11:38:28 +0530

    Create arithmetic.js

---
+++ Intro-to-JavaScript ES6/arithmetic.js
@@ -0,0 +1,14 @@
+// Addition
+var a = 5 + 7;
+// Subtraction
+var b = 22 - 20;
+// Multiplication
+var c = 8 * 8;
+// Division
+var d = 6 / 2;
+// Remainder
+var e = 9 % 6;
+
+var dogAge = prompt("How old is your dog?");
+var humanAge = (dogAge - 2) * 4 + 21;
+alert("Your dog is " + humanAge + " years old in human years.");
