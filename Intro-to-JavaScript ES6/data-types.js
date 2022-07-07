fc5752a173bb995b21218fc7472c4be077bcda07
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-07 08:06:38 +0530

    Create data-types.js

---
+++ Intro-to-JavaScript ES6/data-types.js
@@ -0,0 +1,12 @@
+// String
+name = "Aditya Rawat";
+
+// Number
+num = 123;
+
+// Boolean
+has_scored = false
+is_Winning = true
+
+// In case you want to check for the data type, we can simply use the function typeof().
+typeof(num)
