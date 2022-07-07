24680ba5c13d856885b17cab00be137e9c9cc56b
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-07 20:34:16 +0530

    Create parameters-arguments.js

---
+++ Intro-to-JavaScript ES6/parameters-arguments.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  var c = a + b;
+  console.log("Sum = " + c);
+}
+
+function subtract(a, b) {
+  var c = b - a;
+  console.log("Difference = " + c);
+}
+
+add(4, 6);
+subtract(20, 22);
