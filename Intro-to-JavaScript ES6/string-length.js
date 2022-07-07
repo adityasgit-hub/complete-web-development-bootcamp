15ac3ff37df5b1e6f40cf247d4bfeeb16cfc9841
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-07 08:48:39 +0530

    Create string-length.js

---
+++ Intro-to-JavaScript ES6/string-length.js
@@ -0,0 +1,6 @@
+var name = "Aditya";
+name.length;
+
+var tweet = prompt("Compose your tweet: ");
+var tweetcount = tweet.length;
+alert("You have written " + tweetcount + " characters, you have " + (140 - tweetcount) + " characters left.");
