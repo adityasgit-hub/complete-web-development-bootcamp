7ba85b69702642cd6584065bb5422897087223c1
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-07 21:49:40 +0530

    Create return-values.js

---
+++ Intro-to-JavaScript ES6/return-values.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  c = a + b;
+  return c;
+}
+
+function subtract(a, b) {
+  c = b - a;
+  return c;
+}
+
+var sum = add(4, 6);
+var difference = subtract(16, 24);
+
+console.log("Sum = " + sum);
+console.log("Difference = " + difference);
