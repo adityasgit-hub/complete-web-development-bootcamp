14d5837add3ddbf351b22f2256b781c95041f580
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-07 11:46:52 +0530

    Create increment-decrement.js

---
+++ Intro-to-JavaScript ES6/increment-decrement.js
@@ -0,0 +1,20 @@
+var x = 5;
+// x = x + 1; // x = 6
+x++; // x = 6
+x += 2; // x = 7
+
+y = 3;
+// y = y - 1; // y = 2
+y--; // y = 2
+y -= 2; // y = 1
+
+x += y; // x = 8
+x -= y; // x = 2
+x *= y; // x = 15
+
+// Test
+var x = 3;
+var y = x++;
+y += 1;
+
+// What will be the value of 'y' be?
