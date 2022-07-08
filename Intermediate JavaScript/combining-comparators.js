045b9c11e1f02c313fd647944d9788bdd902d9f9
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-08 14:05:32 +0530

    Create combining-comparators.js

---
+++ Intermediate JavaScript/combining-comparators.js
@@ -0,0 +1,20 @@
+// Combining Comparators: &&, ||, and !.
+
+side1 = prompt("Enter the first side of the triangle: ");
+side2 = prompt("Enter the second side of the triangle: ");
+side3 = prompt("Enter the third side of the triangle: ");
+
+if (((side1 + side2) > side3) && ((side2 + side3) > side1) && ((side3 + side1) > side2)) {
+  if (side1 === side2 && side2 === side3) {
+    console.log("Equilateral Triangle!");
+  }
+  else if (side1 === side2 || side2 === side3 || side3 === side1) {
+    console.log("Isoceles Triangle!");
+  }
+  else {
+    console.log("Scalene Triangle!");
+  }
+}
+else {
+  console.log("Invalid Triangle!");
+}
