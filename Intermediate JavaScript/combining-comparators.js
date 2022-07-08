48c074e22502fcc0b238783bc9e500560ea275bf
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-08 14:06:32 +0530

    Update combining-comparators.js

--- Intermediate JavaScript/combining-comparators.js
+++ Intermediate JavaScript/combining-comparators.js
@@ -1,8 +1,8 @@
 // Combining Comparators: &&, ||, and !.
 
-side1 = prompt("Enter the first side of the triangle: ");
-side2 = prompt("Enter the second side of the triangle: ");
-side3 = prompt("Enter the third side of the triangle: ");
+var side1 = prompt("Enter the first side of the triangle: ");
+var side2 = prompt("Enter the second side of the triangle: ");
+var side3 = prompt("Enter the third side of the triangle: ");
 
 if (((side1 + side2) > side3) && ((side2 + side3) > side1) && ((side3 + side1) > side2)) {
   if (side1 === side2 && side2 === side3) {
