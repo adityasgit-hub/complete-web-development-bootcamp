280a82bcd9ddf913daaad823e0cb225de232ba68
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-09 23:09:35 +0530

    Update fibonacci.js

--- Intermediate JavaScript/fibonacci.js
+++ Intermediate JavaScript/fibonacci.js
@@ -3,23 +3,23 @@ function fibonacciGenerator (n) {
 // Do NOT change any of the code above 👆
     
   // Write your code here:
-  var fibonacci = [];
+  var output = [];
   var a = -1;
   var b = 1;
   var sum = 0;
     
   for (var i = 0; i < n; i += 1) {
       sum = a + b;
-      fibonacci.push(sum);
+      output.push(sum);
       a = b;
       b = sum;
   }
     
   // Return an array of fibonacci numbers starting from 0.
-  return fibonacci;
+  return output;
     
-//Do NOT change any of the code below 👇
+// Do NOT change any of the code below 👇
 }
 
-var fibo = fibonacciGenerator(10);
-console.log(fibo);
+var fibonacci = fibonacciGenerator(10);
+console.log(fibonacci);
