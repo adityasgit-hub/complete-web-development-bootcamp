dafdc84a9ec33c7fd58d68175e4305125f8a5933
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-09 16:44:41 +0530

    Create fizz-buzz-while-loop.js

---
+++ Intermediate JavaScript/fizz-buzz-while-loop.js
@@ -0,0 +1,25 @@
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+
+  while (count <= 100) {
+    
+    if (count % 3 == 0 && count % 5 == 0) {
+      output.push("Fizz Buzz");
+    }
+    else if (count % 3 == 0) {
+      output.push("Fizz");
+    }
+    else if (count % 5 == 0) {
+      output.push("Buzz");
+    }
+    else {
+      output.push(count);
+    }
+    
+    count += 1;
+  }
+  
+   console.log(output);
+}
