9c8a29882a37483ccb8b533e240c926934bc757a
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-09 14:38:46 +0530

    Update russian-roulette.js

--- Intermediate JavaScript/russian-roulette.js
+++ Intermediate JavaScript/russian-roulette.js
@@ -5,7 +5,7 @@ function whosPaying(names) {
     //Write your code here.
     
     var rand = Math.random();
-    rand = Math.floor(rand * 5);
+    rand = Math.floor(rand * names.length);
     return (names[rand] + " is going to buy lunch today!");
 
 /******Don't change the code below*******/    
