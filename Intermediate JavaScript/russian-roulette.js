81fcb8aee91a002c559eddcba8b118bc5164c912
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-09 14:35:45 +0530

    Create russian-roulette.js

---
+++ Intermediate JavaScript/russian-roulette.js
@@ -0,0 +1,12 @@
+function whosPaying(names) {
+
+/******Don't change the code above*******/
+
+    //Write your code here.
+    
+    var rand = Math.random();
+    rand = Math.floor(rand * 5);
+    return (names[rand] + " is going to buy lunch today!");
+
+/******Don't change the code below*******/    
+}
