f193785371da68b99e4c2d2e4acd8315eea49687
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-11 11:00:59 +0530

    Create js-objects.js

---
+++ Advanced JavaScript/js-objects.js
@@ -0,0 +1,12 @@
+// Constructor Function
+function BellBoy (name, age, hasWorkPermit, languages) {
+  this.name = name;
+  this.age = age;
+  this.hasWorkPermit = hasWorkPermit;
+  this.languages = languages;
+}
+
+// Initialise Object
+var bellBoy1 = new BellBoy("John", 20, true, ["English", "French"]);
+var bellBoy2 = new BellBoy("Sean", 25, false, ["Portugese", "English"]);
+var bellBoy3 = new BellBoy("Willian", 21, false, ["Spanish", "German", "Italian"]);
