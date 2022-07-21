b74a3b09a33747b79579978af54effd76d58eff4
Aditya Rawat <adityaajayrawat@gmail.com>
2022-07-21 08:42:37 +0530

    Create script.js

---
+++ jQuery/script.js
@@ -0,0 +1,32 @@
+// Selecting Elements with jQuery
+// So instead of writing 'document.querySelector("h1")', we can write either 'jQuery("h1")' or '$("h1")'.
+
+// Manipulating Styles with jQuery
+// $("h1").css("color", "red");
+// $("h1").css("font-size", "5rem");
+// $("h1").addClass("big-title margin-50");
+// console.log($("h1").hasClass("margin-50"));
+
+// Manipulating Text with jQuery
+// $("h1").text("Bye!")
+// $("button").html("<em>Click Me</em>")
+
+// Manipulating Attributes with jQuery
+// console.log($("img").attr("src"));
+// $("a").attr("href", "https://www.yahoo.com");
+// console.log($("h1").attr("class"));
+
+// Adding Even Listeners with jQuery
+// $("h1").click(function () {$("h1").css("color", "rebeccapurple");});
+// $("button").click(function () {$("h1").css("color", "rebeccapurple");});
+// $(document).keypress(function (event) {$("h1").text(event.key);});
+
+// Adding and Removing Elements with jQuery
+// before | after | prepend | append
+
+// Website Animations with jQuery
+// hide | show | toggle
+// fadeIn | fadeOut | fadeToggle
+// slideUp | slideDown | slideToggle
+// animate (custom animations)
+// we can also combine two or three animations together!
